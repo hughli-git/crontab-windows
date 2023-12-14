1180d362043a904c30f00f098574a414265cdf2b
LiXu <hugh@contek.io>
2023-12-14 12:08:28 +0800

    format: lint

--- contab-windows/my_cron.py
+++ contab-windows/my_cron.py
@@ -1,12 +1,11 @@
-import subprocess
-import time
-from datetime import datetime
 import traceback
-import re
+import time
 import os
 import sys
 import logging
+import subprocess
 import psutil
+from datetime import datetime
 
 
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
@@ -16,68 +15,66 @@ DEFAULT_CONFIG = os.path.join(CURRENT_DIR, "crontab.txt")
 WAIT_PROCESS_TIME = 0.2
 
 
-
 def parse_cron_line(cron_line):
-    '''
+    """
     解析单行crontab记录
-    '''
+    """
     parts = cron_line.strip().split()
     if len(parts) < 6:
         return None, None
-    schedule, command = parts[:5], ' '.join(parts[5:])
-    
+    schedule, command = parts[:5], " ".join(parts[5:])
+
     # 将星号替换成数字范围
-    mins = schedule[0].replace('*', '0-59')
-    hours = schedule[1].replace('*', '0-23')
-    days = schedule[2].replace('*', '1-31')
-    months = schedule[3].replace('*', '1-12')
-    weekdays = schedule[4].replace('*', '0-6')
+    mins = schedule[0].replace("*", "0-59")
+    hours = schedule[1].replace("*", "0-23")
+    days = schedule[2].replace("*", "1-31")
+    months = schedule[3].replace("*", "1-12")
+    weekdays = schedule[4].replace("*", "0-6")
 
     cron_schedule = {
-        'mins': mins,
-        'hours': hours,
-        'days': days,
-        'months': months,
-        'weekdays': weekdays
+        "mins": mins,
+        "hours": hours,
+        "days": days,
+        "months": months,
+        "weekdays": weekdays,
     }
-    
+
     return cron_schedule, command
 
 
-
 def is_time_to_run(cron_schedule):
-    '''
+    """
     判断当前时间是否符合crontab时间设置
-    '''
+    """
     if not cron_schedule:
         return False
     now = datetime.now()
-    if not is_within_range(now.minute, cron_schedule['mins']):
+    if not is_within_range(now.minute, cron_schedule["mins"]):
         return False
-    if not is_within_range(now.hour, cron_schedule['hours']):
+    if not is_within_range(now.hour, cron_schedule["hours"]):
         return False
-    if not is_within_range(now.day, cron_schedule['days']):
+    if not is_within_range(now.day, cron_schedule["days"]):
         return False
-    if not is_within_range(now.month, cron_schedule['months']):
+    if not is_within_range(now.month, cron_schedule["months"]):
         return False
-    if not is_within_range(now.weekday(), cron_schedule['weekdays']):
+    if not is_within_range(now.weekday(), cron_schedule["weekdays"]):
         return False
     return True
 
 
 def is_within_range(current_value, cron_field):
-    '''
+    """
     检查当前值是否在crontab字段范围内
     TODO: 1-10/2 会被识别为 */2
     不识别LW等特殊字符
-    '''
-    for part in cron_field.split(','):
-        if '/' in part:
-            denominator = int(part.split('/')[1])
+    """
+    for part in cron_field.split(","):
+        if "/" in part:
+            denominator = int(part.split("/")[1])
             if current_value % denominator == 0:
                 return True
-        elif '-' in part:
-            start, end = map(int, part.split('-'))
+        elif "-" in part:
+            start, end = map(int, part.split("-"))
             if start <= current_value <= end:
                 return True
         elif int(part) == current_value:
@@ -86,21 +83,23 @@ def is_within_range(current_value, cron_field):
 
 
 def run_command(command):
-    '''
+    """
     执行命令, 启动进程
-    '''
+    """
     try:
         logging.info(f"CMD = {command}")
         # 启动进程
-        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        proc = subprocess.Popen(
+            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        )
         # 等待后看是否直接执行失败
         time.sleep(WAIT_PROCESS_TIME)
         proc.poll()
         # 执行完毕, 且执行失败
         if proc.returncode is not None and proc.returncode != 0:
             logging.info(f"proc.returncode={proc.returncode}")
-            logging.info(proc.stdout.read().decode('gbk').strip())
-        logging.info(f"start CMD finish")
+            logging.info(proc.stdout.read().decode("gbk").strip())
+        logging.info("start CMD finish")
     except Exception:
         exc = traceback.format_exc()
         logging.error(exc)
@@ -129,9 +128,9 @@ def main_loop(config_file):
 
 
 def is_process_running():
-    '''
+    """
     判断是否已有进程启动
-    '''
+    """
     # 获取当前进程及父进程列表
     my_pid = os.getpid()
     my_pid_list = set()
@@ -156,8 +155,8 @@ def is_process_running():
         try:
             p = psutil.Process(one_pid)
             p_cmd = p.cmdline()
-            cmd_line = ' '.join(p_cmd).lower()
-            if py_file_name.lower() in cmd_line and 'python' in cmd_line:
+            cmd_line = " ".join(p_cmd).lower()
+            if py_file_name.lower() in cmd_line and "python" in cmd_line:
                 logging.info(f"pid={one_pid}, cmd={cmd_line}")
                 process_count += 1
         except BaseException:
@@ -172,9 +171,9 @@ if __name__ == "__main__":
     config_path = DEFAULT_CONFIG
     if len(sys.argv) == 2:
         config_path = sys.argv[1]
-    
+
     # 校验配置文件路径
     if not os.path.exists(config_path):
         raise ValueError(f"Invalid crontab file: {config_path}")
-    
+
     main_loop(config_path)
